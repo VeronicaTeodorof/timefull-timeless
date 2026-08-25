2e4332f5b6e8e02f48be57f1291ded5590baecc9
VeronicaTeodorof <vteodorof@rocketmail.com>
2026-08-25 09:09:32 +0100

    chore: configure Cloudinary for deployment

--- timefull_timeless/settings.py
+++ timefull_timeless/settings.py
@@ -27,6 +27,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
+# Cloudinary
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+}
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
@@ -49,6 +56,7 @@ INSTALLED_APPS = [
     'gallery',
     'checkout',
     'cloudinary',
+    'cloudinary_storage',
 ]
 
 MIDDLEWARE = [
