ac5fc67a9bef56c96a3521f206ea56943c05e26a
VeronicaTeodorof <vteodorof@rocketmail.com>
2026-09-04 14:49:26 +0100

    feat: add DeliveryCost model

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+class DeliveryCost(models.Model):
+    """
+    A model that allows business owner to adjust delivery cost
+    """
+    class Country(models.TextChoices):
+        RO = "RO", "Romania"
+        UK = "UK", "United Kingdom"
+    country = models.CharField(max_length=2,
+                               choices=Country.choices,
+                               default=Country.UK)
+    cost = models.DecimalField(
+        max_digits=5,
+        decimal_places=2)
