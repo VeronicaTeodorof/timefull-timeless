826b45c83b38e0ced830db955529d05370ff1a8c
VeronicaTeodorof <vteodorof@rocketmail.com>
2026-09-05 22:54:33 +0100

    feat: add hardcoded cost to terms.html

--- checkout/views.py
+++ checkout/views.py
@@ -3,6 +3,7 @@ from gallery.models import Sculpture
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import DeliveryCost
+from pages.models import BusinessSettings
 
 
 # Create your views here.
@@ -18,8 +19,12 @@ def terms_view(request, sculpture_slug):
     sculpture = get_object_or_404(Sculpture, slug=sculpture_slug)
     uk_cost = DeliveryCost.objects.get(country='UK')
     ro_cost = DeliveryCost.objects.get(country='RO')
+    business_settings = BusinessSettings.load()
+    insurance_cost = round(sculpture.price * business_settings.insurance_rate, 2)
     return render(request,
                   'checkout/terms.html',
                   {'sculpture': sculpture,
                    'uk_cost': uk_cost,
-                   'ro_cost': ro_cost, })
+                   'ro_cost': ro_cost,
+                   'insurance_cost': insurance_cost, })
+
