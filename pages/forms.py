03f5f087f15df9f885f678b50cb8ff46a8bf485a
VeronicaTeodorof <vteodorof@rocketmail.com>
2026-09-11 17:08:54 +0100

    feat: implement ContactForm fields

--- pages/forms.py
+++ pages/forms.py
@@ -5,4 +5,8 @@ class ContactForm(forms.Form):
     """
     A form for the contact page
     """
-    pass
+    name = forms.CharField(max_length=100)
+    email = forms.EmailField()
+    phone = forms.CharField(max_length=30, required=False)
+    subject = forms.CharField(max_length=150, required=False)
+    message = forms.CharField(widget=forms.Textarea)
